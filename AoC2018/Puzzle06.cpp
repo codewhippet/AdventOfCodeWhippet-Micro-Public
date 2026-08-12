824ac0b3b3cfbad7580f4abcfe0b66be1661dec8
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-08-12 10:26:20 +0100

    2018 day 6 optimisation We weren't saving anything by performing a complex search compared to just scanning over the whole array and doing a per-region distance check. Simplified the whole approach to more-or-less brute force it. ~1,050ms -> ~767ms

--- AoC2018/Puzzle06.cpp
+++ AoC2018/Puzzle06.cpp
@@ -2,177 +2,36 @@
 
 using namespace std;
 
-static string_view dummy =
-R"()";
-
 namespace Puzzle06_2018_Types
 {
-	struct CoordinateSpace
-	{
-		vector<Vec2Int> Coords;
-		int32_t Left;
-		int32_t Right;
-		int32_t Top;
-		int32_t Bottom;
-	};
-
-	class Bitfield2D
-	{
-	public:
-		Bitfield2D()
-			: Storage((512 * 512) >> 5)
-		{
-		}
-
-		bool GetAndSet(const Vec2Int& pos)
-		{
-			assert((pos.X & ~0x1ff) == 0);
-			assert((pos.Y & ~0x1ff) == 0);
-
-			size_t index = (pos.X >> 5) | (pos.Y << 4);
-			uint32_t shift = pos.X & 0x1f;
-
-			uint32_t bits = Storage[index];
-			uint32_t bit = (1 << shift);
-
-			bool wasAlreadySet = (bits & bit);
-			bits |= bit;
-			Storage[index] = bits;
-
-			return wasAlreadySet;
-		}
-
-	private:
-
-		vector<uint32_t> Storage;
-	};
 }
 
 using namespace Puzzle06_2018_Types;
 
-static CoordinateSpace ReadCoordinates()
-{
-	CoordinateSpace ret;
-	ret.Coords.reserve(64);
-
-	ret.Left = numeric_limits<int32_t>::max();
-	ret.Right = numeric_limits<int32_t>::min();
-	ret.Top = numeric_limits<int32_t>::max();
-	ret.Bottom = numeric_limits<int32_t>::min();
-
-	while (PuzzleInput::NextLine())
-	{
-		Vec2Int c{ Parse::GetInt32(), Parse::GetInt32() };
-		ret.Left = min(ret.Left, c.X);
-		ret.Right = max(ret.Right, c.X);
-		ret.Top = min(ret.Top, c.Y);
-		ret.Bottom = max(ret.Bottom, c.Y);
-		ret.Coords.push_back(c);
-	}
-
-	return ret;
-}
-
-static void TagInfiniteRegions(const CoordinateSpace& space, const Vec2Int& edgeFrom, const Vec2Int& edgeTo, vector<bool>* regions)
+static void TagInfiniteRegions(const vector<Vec2Int>& coords, const Vec2Int& edgeFrom, const Vec2Int& edgeTo, vector<bool>* regions)
 {
 	for (Vec2Int pos : uLineInclusiveRange{ edgeFrom, edgeTo })
 	{
 		int32_t nearestDistance = numeric_limits<int32_t>::max();
 		size_t nearestTo = numeric_limits<size_t>::max();
-		int32_t nearestCount = 0;
-		for (size_t i = 0; i < space.Coords.size(); i++)
+		for (size_t i = 0; i < coords.size(); i++)
 		{
-			int32_t distance = ManhattanDistance(pos, space.Coords[i]);
+			int32_t distance = ManhattanDistance(pos, coords[i]);
 			if (distance < nearestDistance)
 			{
 				nearestDistance = distance;
 				nearestTo = i;
-				nearestCount = 1;
 			}
 			else if (distance == nearestDistance)
 			{
-				nearestCount++;
+				nearestTo = numeric_limits<size_t>::max();
 			}
 		}
-		if (nearestCount == 1)
+		if (nearestTo != numeric_limits<size_t>::max())
 		{
 			(*regions)[nearestTo] = false;
 		}
 	}
-}
-
-static vector<size_t> GetFiniteRegions(const CoordinateSpace& space)
-{
-	vector<bool> isFiniteRegion(space.Coords.size(), true);
-
-	TagInfiniteRegions(space, Vec2Int{ space.Left, space.Top }, Vec2Int{ space.Right, space.Top }, &isFiniteRegion); // Across the top
-	TagInfiniteRegions(space, Vec2Int{ space.Left, space.Top }, Vec2Int{ space.Left, space.Bottom }, &isFiniteRegion); // Down left
-	TagInfiniteRegions(space, Vec2Int{ space.Right, space.Top }, Vec2Int{ space.Right, space.Bottom }, &isFiniteRegion); // Down right
-	TagInfiniteRegions(space, Vec2Int{ space.Left, space.Bottom }, Vec2Int{ space.Right, space.Bottom }, &isFiniteRegion); // Across the bottom
-
-	vector<size_t> finiteRegions;
-	finiteRegions.reserve(space.Coords.size());
-	for (size_t i = 0; i < space.Coords.size(); i++)
-	{
-		if (isFiniteRegion[i])
-		{
-			finiteRegions.push_back(i);
-		}
-	}
-
-	return finiteRegions;
-}
-
-static size_t SizeOfRegion(const vector<Vec2Int>& regionCentres)
-{
-	vector<Vec2Int> searchQueue;
-	searchQueue.reserve(4 * 1024);
-	searchQueue.push_back(regionCentres[0]);
-
-	Bitfield2D evaluated;
-	evaluated.GetAndSet(regionCentres[0]);
-
-	for (size_t searchIndex = 0; searchIndex < searchQueue.size(); searchIndex++)
-	{
-		Vec2Int current = searchQueue[searchIndex];
-		for (const Vec2Int& dir : Vec2Int::CardinalDirections())
-		{
-			Vec2Int neighbour = current + dir;
-			if (evaluated.GetAndSet(neighbour) == false)
-			{
-				MinValue<int32_t> minDistanceToOther;
-				for (size_t i = 1; i < regionCentres.size(); i++)
-				{
-					int32_t distanceToOther = ManhattanDistance(regionCentres[i], neighbour);
-					minDistanceToOther.Update(distanceToOther);
-				}
-
-				int32_t distanceToRegionCentre = ManhattanDistance(regionCentres[0], neighbour);
-				if (distanceToRegionCentre < minDistanceToOther)
-				{
-					searchQueue.push_back(neighbour);
-				}
-			}
-		}
-	}
-
-	return searchQueue.size();
-}
-
-static size_t LargestFiniteRegion(const CoordinateSpace& space, const vector<size_t>& finiteRegions)
-{
-	vector<Vec2Int> regionCentres = space.Coords;
-
-	MaxValue<size_t> largestSize;
-	for (size_t region : finiteRegions)
-	{
-		swap(regionCentres[0], regionCentres[region]);
-		size_t regionSize = SizeOfRegion(regionCentres);
-		largestSize.Update(regionSize);
-		swap(regionCentres[0], regionCentres[region]);
-	}
-
-	return largestSize;
 }
 
 static vector<int32_t> GetDistances(const vector<int32_t>& axis, int32_t rangeBegin, int32_t rangeEnd, int32_t maxDistance)
@@ -198,12 +57,74 @@ static vector<int32_t> GetDistances(const vector<int32_t>& axis, int32_t rangeBe
 
 void Puzzle06_A_2018()
 {
-	CoordinateSpace space = ReadCoordinates();
-	vector<size_t> finiteRegions = GetFiniteRegions(space);
-	size_t answer = LargestFiniteRegion(space, finiteRegions);
+	vector<Vec2Int> coords;
+	coords.reserve(64);
 
+	MinMaxValues<int32_t> xMinMax, yMinMax;
+	while (PuzzleInput::NextLine())
+	{
+		int32_t x = Parse::GetInt32();
+		int32_t y = Parse::GetInt32();
+
+		xMinMax.Update(x);
+		yMinMax.Update(y);
+
+		coords.push_back({ x, y });
+	}
+
+	vector<bool> isFiniteRegion(coords.size(), true);
+
+	TagInfiniteRegions(coords, Vec2Int{ xMinMax.GetMin(), yMinMax.GetMin() }, Vec2Int{ xMinMax.GetMax(), yMinMax.GetMin() }, &isFiniteRegion); // Across the top
+	TagInfiniteRegions(coords, Vec2Int{ xMinMax.GetMin(), yMinMax.GetMin() }, Vec2Int{ xMinMax.GetMin(), yMinMax.GetMax() }, &isFiniteRegion); // Down left
+	TagInfiniteRegions(coords, Vec2Int{ xMinMax.GetMax(), yMinMax.GetMin() }, Vec2Int{ xMinMax.GetMax(), yMinMax.GetMax() }, &isFiniteRegion); // Down right
+	TagInfiniteRegions(coords, Vec2Int{ xMinMax.GetMin(), yMinMax.GetMax() }, Vec2Int{ xMinMax.GetMax(), yMinMax.GetMax() }, &isFiniteRegion); // Across the bottom
+
+	vector<int32_t> regionSizes(coords.size(), 0);
+
+	vector<int32_t> yDistances(coords.size(), -1);
+	for (int32_t y = yMinMax.GetMin() + 1; y < yMinMax.GetMax(); y++)
+	{
+		for (size_t i = 0; i < coords.size(); i++)
+		{
+			yDistances[i] = abs(coords[i].Y - y);
+		}
+
+		for (int32_t x = xMinMax.GetMin() + 1; x < xMinMax.GetMax(); x++)
+		{
+			int32_t nearestRegionDistance = numeric_limits<int32_t>::max();
+			size_t nearestRegion = numeric_limits<size_t>::max();
+			for (size_t i = 0; i < coords.size(); i++)
+			{
+				int32_t regionDistance = abs(coords[i].X - x) + yDistances[i];
+				if (regionDistance < nearestRegionDistance)
+				{
+					nearestRegionDistance = regionDistance;
+					nearestRegion = static_cast<int32_t>(i);
+				}
+				else if (regionDistance == nearestRegionDistance)
+				{
+					nearestRegion = numeric_limits<size_t>::max();
+				}
+			}
+
+			if (nearestRegion != numeric_limits<size_t>::max())
+			{
+				regionSizes[nearestRegion]++;
+			}
+		}
+	}
+
+	MaxValue<int32_t> largestFiniteRegion;
+	for (size_t i = 0; i < regionSizes.size(); i++)
+	{
+		if (isFiniteRegion[i])
+		{
+			largestFiniteRegion.Update(regionSizes[i]);
+		}
+	}
+
+	int32_t answer = largestFiniteRegion.Get();
 	PuzzleOutput::Submit(2018, 6, 1, answer);
-
 }
 
 void Puzzle06_B_2018()
