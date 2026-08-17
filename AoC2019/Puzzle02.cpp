63a1104b097d67805f260629b10fc424d5259537
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-08-17 21:26:05 +0100

    2019 day 2 squashed 64-bit -> 32-bit

--- AoC2019/Puzzle02.cpp
+++ AoC2019/Puzzle02.cpp
@@ -2,23 +2,20 @@
 
 using namespace std;
 
-static string_view dummy =
-R"()";
-
 namespace Puzzle02_2019_Types
 {
 }
 
 using namespace Puzzle02_2019_Types;
 
-static void Execute(vector<int64_t>* program)
+static void Execute(vector<int32_t>* program)
 {
-	vector<int64_t>& p = *program;
+	vector<int32_t>& p = *program;
 
-	int64_t pc = 0;
+	int32_t pc = 0;
 	while (true)
 	{
-		int64_t instruction = p[pc];
+		int32_t instruction = p[pc];
 		switch (instruction)
 		{
 		case 99:
@@ -26,8 +23,8 @@ static void Execute(vector<int64_t>* program)
 
 		case 1:
 			{
-				int64_t a = p[p[pc + 1]];
-				int64_t b = p[p[pc + 2]];
+				int32_t a = p[p[pc + 1]];
+				int32_t b = p[p[pc + 2]];
 				p[p[pc + 3]] = a + b;
 				pc += 4;
 			}
@@ -35,8 +32,8 @@ static void Execute(vector<int64_t>* program)
 
 		case 2:
 			{
-				int64_t a = p[p[pc + 1]];
-				int64_t b = p[p[pc + 2]];
+				int32_t a = p[p[pc + 1]];
+				int32_t b = p[p[pc + 2]];
 				p[p[pc + 3]] = a * b;
 				pc += 4;
 			}
@@ -45,7 +42,7 @@ static void Execute(vector<int64_t>* program)
 	}
 }
 
-static int64_t GravityAssist(vector<int64_t> program, int64_t a, int64_t b)
+static int32_t GravityAssist(vector<int32_t> program, int32_t a, int32_t b)
 {
 	program[1] = a;
 	program[2] = b;
@@ -53,11 +50,11 @@ static int64_t GravityAssist(vector<int64_t> program, int64_t a, int64_t b)
 	return program[0];
 }
 
-static int64_t FindGravityAssist(const vector<int64_t> program)
+static int32_t FindGravityAssist(const vector<int32_t>& program)
 {
-	for (int64_t noun : views::iota(0, 100))
+	for (int32_t noun : views::iota(0, 100))
 	{
-		for (int64_t verb : views::iota(0, 100))
+		for (int32_t verb : views::iota(0, 100))
 		{
 			if (GravityAssist(program, noun, verb) == 19690720)
 			{
@@ -68,41 +65,34 @@ static int64_t FindGravityAssist(const vector<int64_t> program)
 	return -1;
 }
 
-static void Puzzle02_A(const string &filename)
+void Puzzle02_A_2019()
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
+	vector<int32_t> program;
+	program.reserve(200);
+	while (PuzzleInput::NextLine())
+	{
+		program.push_back(Parse::GetInt32());
+	}
 
-	auto program = ReadAsVectorOfNumbers(ReadSingleLine(input));
 	program[1] = 12;
 	program[2] = 2;
 
 	Execute(&program);
 
-	int64_t answer = program[0];
-
-	printf("[2019] Puzzle02_A: %" PRId64 "\n", answer);
-}
-
-static void Puzzle02_B(const string& filename)
-{
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	auto program = ReadAsVectorOfNumbers(ReadSingleLine(input));
-	int64_t answer = FindGravityAssist(program);
-
-	printf("[2019] Puzzle02_B: %" PRId64 "\n", answer);
-}
-
-void Puzzle02_A_2019()
-{
-	Puzzle02_A(R"(z:\AoCInput\2019\Puzzle02.txt)");
+	int32_t answer = program[0];
+	PuzzleOutput::Submit(2019, 2, 1, answer);
 }
 
 void Puzzle02_B_2019()
 {
-	Puzzle02_B(R"(z:\AoCInput\2019\Puzzle02.txt)");
+	vector<int32_t> program;
+	program.reserve(200);
+	while (PuzzleInput::NextLine())
+	{
+		program.push_back(Parse::GetInt32());
+	}
+
+	int32_t answer = FindGravityAssist(program);
+
+	PuzzleOutput::Submit(2019, 2, 1, answer);
 }
