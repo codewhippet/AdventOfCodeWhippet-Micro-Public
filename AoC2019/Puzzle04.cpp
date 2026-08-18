4497caf68fcd4b15441adc1a3d336b6642435e66
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-08-18 20:37:06 +0100

    2019 day 4 squashed Use explicit digit incrementing rather than sprintf. Tidied up checks for password conditions. PC timings ~80ms -> ~0.4ms for part 2

--- AoC2019/Puzzle04.cpp
+++ AoC2019/Puzzle04.cpp
@@ -11,87 +11,117 @@ namespace Puzzle04_2019_Types
 
 using namespace Puzzle04_2019_Types;
 
-static bool IsValidPasswordPart1(const string& s)
+static void IncrementDigits(char* digits, char* end)
 {
-	bool hasDouble = false;
-	bool decreases = false;
-	for (size_t i = 0; (i + 1) < s.size(); i++)
+	char incremented = ((*digits) += 1);
+	if (incremented <= '9')
 	{
-		if (s[i] == s[i + 1])
-			hasDouble = true;
-		if (s[i] > s[i + 1])
-			decreases = true;
+		return;
 	}
-	return hasDouble && !decreases;
+	(*digits--) -= 10;
+
+	while (digits != end)
+	{
+		incremented = ++(*digits);
+		if (incremented <= '9')
+		{
+			return;
+		}
+		*digits-- = '0';
+	}
 }
 
-static bool IsValidPasswordPart2(const string& s)
+static bool IsMonotonic(const char* digits, size_t numDigits)
 {
-	vector<char> diffs(s.size() - 1);
-	diffs.push_back(100);
-	for (size_t i = 0; (i + 1) < s.size(); i++)
+	for (size_t i = 1; i < numDigits; i++)
 	{
-		diffs.push_back(s[i + 1] - s[i]);
+		if (digits[i - 1] > digits[i])
+			return false;
 	}
-	diffs.push_back(100);
 
-	bool hasSingletonZero = false;
-	for (size_t i = 1; (i + 1) < diffs.size(); i++)
+	return true;
+}
+
+static bool HasDouble(const char* digits, size_t numDigits)
+{
+	for (size_t i = 1; i < numDigits; i++)
 	{
-		if ((diffs[i] == 0) &&
-			(diffs[i - 1] != 0) &&
-			(diffs[i + 1] != 0))
+		if (digits[i - 1] == digits[i])
+			return true;
+	}
+
+	return false;
+}
+
+static bool HasIsolatedDouble(const char* digits, size_t numDigits)
+{
+	for (size_t i = 1; i < numDigits; i++)
+	{
+		if (digits[i - 2] != digits[i - 1] &&
+			digits[i - 1] == digits[i] &&
+			digits[i] != digits[i + 1])
 		{
-			hasSingletonZero = true;
+			return true;
 		}
 	}
 
-	return hasSingletonZero && ranges::none_of(diffs, [](char c) { return c < 0; });
-}
-
-static void Puzzle04_A(const string &filename)
-{
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	int64_t start, stop;
-	int scanned = sscanf(ReadSingleLine(input).c_str(), "%lld-%lld", &start, &stop);
-	assert(scanned == 2);
-	(void)scanned;
-
-	int64_t answer = (int64_t)ranges::distance(views::iota(start, stop + 1)
-		| views::transform([](int64_t i) { return to_string(i); })
-		| views::filter(IsValidPasswordPart1));
-
-	printf("[2019] Puzzle04_A: %" PRId64 "\n", answer);
-}
-
-
-static void Puzzle04_B(const string& filename)
-{
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	int64_t start, stop;
-	int scanned = sscanf(ReadSingleLine(input).c_str(), "%lld-%lld", &start, &stop);
-	assert(scanned == 2);
-	(void)scanned;
-
-	int64_t answer = (int64_t)ranges::distance(views::iota(start, stop + 1)
-		| views::transform([](int64_t i) { return to_string(i); })
-		| views::filter(IsValidPasswordPart2));
-
-	printf("[2019] Puzzle04_B: %" PRId64 "\n", answer);
+	return false;
 }
 
 void Puzzle04_A_2019()
 {
-	Puzzle04_A(R"(z:\AoCInput\2019\Puzzle04.txt)");
+	const int32_t from = Parse::GetInt32();
+	PuzzleInput::DropChar();
+	const int32_t to = Parse::GetInt32();
+
+	assert(from >= 100000);
+	assert(to <= 999999);
+	const size_t numDigits = 6;
+
+	array<char, numDigits + 2> digitsBuffer = {};
+	char* digits = &digitsBuffer[1];
+
+	sprint_digits(digits, from, numDigits);
+
+	int32_t answer = 0;
+	for (int32_t i = from; i <= to; i++)
+	{
+		if (IsMonotonic(digits, numDigits) && HasDouble(digits, numDigits))
+		{
+			answer++;
+		}
+
+		IncrementDigits(&digitsBuffer[numDigits], &digitsBuffer[0]);
+	}
+
+	PuzzleOutput::Submit(2019, 4, 1, answer);
 }
 
 void Puzzle04_B_2019()
 {
-	Puzzle04_B(R"(z:\AoCInput\2019\Puzzle04.txt)");
+	const int32_t from = Parse::GetInt32();
+	PuzzleInput::DropChar();
+	const int32_t to = Parse::GetInt32();
+
+	assert(from >= 100000);
+	assert(to <= 999999);
+	const size_t numDigits = 6;
+
+	array<char, numDigits + 2> digitsBuffer = {};
+	char* digits = &digitsBuffer[1];
+
+	sprint_digits(digits, from, numDigits);
+
+	int32_t answer = 0;
+	for (int32_t i = from; i <= to; i++)
+	{
+		if (IsMonotonic(digits, numDigits) && HasIsolatedDouble(digits, numDigits))
+		{
+			answer++;
+		}
+
+		IncrementDigits(&digitsBuffer[numDigits], &digitsBuffer[0]);
+	}
+
+	PuzzleOutput::Submit(2019, 4, 2, answer);
 }
