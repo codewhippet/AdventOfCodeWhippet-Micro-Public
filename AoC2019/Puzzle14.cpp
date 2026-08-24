f93a3f638668588f3a56be7d3a6850afd13955de
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-08-24 15:21:46 +0100

    2019 day 14 squashed Use IDs rather than strings to identify chemicals. Prefer vectors and HashMaps instead of std::maps

--- AoC2019/Puzzle14.cpp
+++ AoC2019/Puzzle14.cpp
@@ -2,38 +2,43 @@
 
 using namespace std;
 
-static string_view dummy =
-R"()";
-
 namespace Puzzle14_2019_Types
 {
 	struct Reaction
 	{
-		string Chemical;
-		int64_t Created;
-		vector<pair<string, int64_t>> Requires;
+		int32_t Chemical = -1;
+		int32_t Created;
+		vector<pair<int32_t, int32_t>> Requires;
 	};
 }
 
 using namespace Puzzle14_2019_Types;
 
-static map<string, Reaction> ParseReactions(istream& input)
+static vector<Reaction> ParseReactions(NameDictionary* names)
 {
+	vector<Reaction> reactions;
+	reactions.resize(64); // We're going to have some slop at the top with junk reactions
+
 	const regex reaction{ R"(([^=]+) => (\d+) (\w+))" };
 	const regex chemical{ R"((\d+) (\w+))" };
 
-	map<string, Reaction> reactions;
-	for (const auto& lineMatch : ReadEachLine(input, reaction))
+	char line[128];
+	while (PuzzleInput::NextLine())
 	{
+		Parse::ReadNonEmptyLine(line);
+
+		cmatch lineMatch;
+		regex_match(line, lineMatch, reaction);
+
 		Reaction r;
-		r.Chemical = lineMatch[3].str();
-		r.Created = atoll(lineMatch[2].str().c_str());
+		r.Chemical = names->IdFromName(lineMatch[3].str().c_str());
+		r.Created = atoi(lineMatch[2].str().c_str());
 
 		string precursors = lineMatch[1].str();
 		auto precursors_begin = sregex_iterator(precursors.begin(), precursors.end(), chemical);
 		for (sregex_iterator chem = precursors_begin; chem != sregex_iterator{}; ++chem)
 		{
-			r.Requires.push_back({ (*chem)[2].str(), atoll((*chem)[1].str().c_str()) });
+			r.Requires.push_back({ names->IdFromName((*chem)[2].str().c_str()), atoi((*chem)[1].str().c_str()) });
 		}
 		reactions[r.Chemical] = r;
 	}
@@ -41,27 +46,33 @@ static map<string, Reaction> ParseReactions(istream& input)
 	return reactions;
 }
 
-static int64_t OreRequiredForFuel(int64_t fuel, const map<string, Reaction>& reactions)
+static int64_t OreRequiredForFuel(int64_t fuel, const vector<Reaction>& reactions, const NameDictionary& names)
 {
-	map<string, int64_t> required;
-	map<string, int64_t> used;
-	map<string, int64_t> overspill;
+	vector<int64_t> required(reactions.size());
+	vector<int64_t> used(reactions.size());
+	vector<int64_t> overspill(reactions.size());
 
-	required["FUEL"] = fuel;
+	required[names.IdFromName("FUEL")] = fuel;
+	const int32_t ORE = names.IdFromName("ORE");
 
 	int64_t oreNeeded = 0;
-	while (required.empty() == false)
+	while (true)
 	{
-		auto [chemical, amountNeeded] = *required.begin();
-		required.erase(required.begin());
+		auto nonEmpty = ranges::find_if(required, [](int64_t v) { return v != 0; });
+		if (nonEmpty == required.end())
+			break;
 
-		if (chemical == "ORE"sv)
+		int32_t chemical = static_cast<int32_t>(distance(required.begin(), nonEmpty));
+		int64_t amountNeeded = *nonEmpty;
+		*nonEmpty = 0;
+
+		if (chemical == ORE)
 		{
 			oreNeeded += amountNeeded;
 			continue;
 		}
 
-		const Reaction& reaction = reactions.at(chemical);
+		const Reaction& reaction = reactions[chemical];
 
 		int64_t numReactions = ((amountNeeded % reaction.Created == 0) ? amountNeeded : (amountNeeded + reaction.Created)) / reaction.Created;
 		int64_t amountCreated = numReactions * reaction.Created;
@@ -86,31 +97,25 @@ static int64_t OreRequiredForFuel(int64_t fuel, const map<string, Reaction>& rea
 		// Top up the overspill with the excess
 		overspill[chemical] += amountCreated - amountNeeded;
 	}
+
 	return oreNeeded;
 }
 
-static void Puzzle14_A(const string &filename)
+void Puzzle14_A_2019()
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
+	NameDictionary names(256);
+	vector<Reaction> reactions = ParseReactions(&names);
+	int64_t answer = OreRequiredForFuel(1, reactions, names);
 
-	map<string, Reaction> reactions = ParseReactions(input);
-	int64_t answer = OreRequiredForFuel(1, reactions);
-
-	printf("[2019] Puzzle14_A: %" PRId64 "\n", answer);
+	PuzzleOutput::Submit(2019, 14, 1, answer);
 }
 
-
-static void Puzzle14_B(const string& filename)
+void Puzzle14_B_2019()
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
 	const int64_t maximumOre = 1000000000000;
 
-	map<string, Reaction> reactions = ParseReactions(input);
+	NameDictionary names(256);
+	vector<Reaction> reactions = ParseReactions(&names);
 
 	int64_t answer = 0;
 
@@ -119,8 +124,8 @@ static void Puzzle14_B(const string& filename)
 	while (true)
 	{
 		int64_t midpointFuel = (lowerFuel + upperFuel) / 2;
-		int64_t oreAtMidpoint = OreRequiredForFuel(midpointFuel, reactions);
-		int64_t oreAboveMidpoint = OreRequiredForFuel(midpointFuel + 1, reactions);
+		int64_t oreAtMidpoint = OreRequiredForFuel(midpointFuel, reactions, names);
+		int64_t oreAboveMidpoint = OreRequiredForFuel(midpointFuel + 1, reactions, names);
 
 		if ((oreAtMidpoint <= maximumOre) && (oreAboveMidpoint > maximumOre))
 		{
@@ -138,21 +143,5 @@ static void Puzzle14_B(const string& filename)
 		}
 	}
 
-	printf("[2019] Puzzle14_B: %" PRId64 "\n", answer);
-}
-
-void Puzzle14_A_2019()
-{
-	Puzzle14_A(R"(z:\AoCInput\2019\Puzzle14.txt)");
-
-	int32_t answer = 0;
-	PuzzleOutput::Submit(2019, 14, 1, answer);
-}
-
-void Puzzle14_B_2019()
-{
-	Puzzle14_B(R"(z:\AoCInput\2019\Puzzle14.txt)");
-
-	int32_t answer = 0;
 	PuzzleOutput::Submit(2019, 14, 2, answer);
 }
