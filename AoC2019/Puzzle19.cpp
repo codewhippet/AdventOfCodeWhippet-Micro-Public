7a443bc67db7322ca7f368db86ea9cac3606ffea
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-08-31 17:04:02 +0100

    2019 day 19 squashed Switched to 32-bit VM

--- AoC2019/Puzzle19.cpp
+++ AoC2019/Puzzle19.cpp
@@ -3,16 +3,13 @@
 
 using namespace std;
 
-static string_view dummy =
-R"()";
-
 namespace Puzzle19_2019_Types
 {
 }
 
 using namespace Puzzle19_2019_Types;
 
-static bool IsInside(const Point2& pos, const vector<int64_t> &program, Intputer* puter)
+static bool IsInside(const Vec2Int& pos, const vector<int32_t>& program, uIntputer<int32_t>* puter)
 {
 	puter->Reset(program);
 
@@ -20,84 +17,79 @@ static bool IsInside(const Point2& pos, const vector<int64_t> &program, Intputer
 	puter->GetReadQueue()->push_back(pos.Y);
 
 	auto exec = puter->Execute();
-	assert(exec == Intputer::ExecutionResult::Finished);
+	assert(exec == uIntputerExecutionResult::Finished);
 	(void)exec;
 
 	return puter->GetWriteQueue()->front() == 1;
 }
 
-static void Puzzle19_A(const string &filename)
+void Puzzle19_A_2019()
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
+	vector<int32_t> program;
+	program.reserve(512);
+	while (PuzzleInput::NextLine())
+	{
+		if (PuzzleInput::PeekChar() == '\n')
+			break;
 
-	vector<int64_t> program = ReadAsVectorOfNumbers(ReadSingleLine(input));
+		program.push_back(Parse::GetInt32());
+	}
+	assert(program.size() < 512);
+	program.resize(512);
 
-	Intputer puter;
+	uIntputer<int32_t> puter;
 
-	IntputerIO io;
+	uIntputerIO<int32_t> io;
 	puter.SetReadWriteQueues(&io);
 
-	int64_t answer = ranges::count_if(GridRange{ Point2{}, Point2{ 50, 50 } },
+	int32_t answer = (int32_t)ranges::count_if(uGridRange{ Vec2Int{}, Vec2Int{ 50, 50 } },
 		[&](const auto& p)
 		{
 			return IsInside(p, program, &puter);
 		});
 
-	printf("[2019] Puzzle19_A: %" PRId64 "\n", answer);
-}
-
-
-static void Puzzle19_B(const string& filename)
-{
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	vector<int64_t> program = ReadAsVectorOfNumbers(ReadSingleLine(input));
-
-	Intputer puter;
-
-	IntputerIO io;
-	puter.SetReadWriteQueues(&io);
-
-	Point2 topRight{ 10, 0 };
-	while (IsInside(topRight, program, &puter) == false)
-		topRight += Point2::Down();
-
-	Point2 closest;
-	while (true)
-	{
-		assert(IsInside(topRight, program, &puter));
-
-		Point2 bottomLeft = topRight + 99 * (Point2::Left() + Point2::Down());
-		if (IsInside(bottomLeft, program, &puter))
-		{
-			closest = Point2{ bottomLeft.X, topRight.Y };
-			break;
-		}
-
-		topRight += IsInside(topRight + Point2::Right(), program, &puter) ? Point2::Right() : Point2::Down();
-	}
-
-	int64_t answer = closest.X * 10000 + closest.Y;
-
-	printf("[2019] Puzzle19_B: %" PRId64 "\n", answer);
-}
-
-void Puzzle19_A_2019()
-{
-	Puzzle19_A(R"(z:\AoCInput\2019\Puzzle19.txt)");
-
-	int32_t answer = 0;
 	PuzzleOutput::Submit(2019, 19, 1, answer);
 }
 
 void Puzzle19_B_2019()
 {
-	Puzzle19_B(R"(z:\AoCInput\2019\Puzzle19.txt)");
+	vector<int32_t> program;
+	program.reserve(512);
+	while (PuzzleInput::NextLine())
+	{
+		if (PuzzleInput::PeekChar() == '\n')
+			break;
 
-	int32_t answer = 0;
+		program.push_back(Parse::GetInt32());
+	}
+	assert(program.size() < 512);
+	program.resize(512);
+
+	uIntputer<int32_t> puter;
+
+	uIntputerIO<int32_t> io;
+	puter.SetReadWriteQueues(&io);
+
+	Vec2Int topRight{ 10, 0 };
+	while (IsInside(topRight, program, &puter) == false)
+		topRight += Vec2Int::Down();
+
+	Vec2Int closest;
+	while (true)
+	{
+		assert(IsInside(topRight, program, &puter));
+
+		Vec2Int bottomLeft = topRight + 99 * (Vec2Int::Left() + Vec2Int::Down());
+		if (IsInside(bottomLeft, program, &puter))
+		{
+			closest = Vec2Int{ bottomLeft.X, topRight.Y };
+			break;
+		}
+
+		topRight += IsInside(topRight + Vec2Int::Right(), program, &puter) ? Vec2Int::Right() : Vec2Int::Down();
+	}
+
+	int32_t answer = closest.X * 10000 + closest.Y;
+
 	PuzzleOutput::Submit(2019, 19, 2, answer);
 }
