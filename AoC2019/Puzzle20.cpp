c5ae87829d3fb7982d691c97bfc34b25b430bcd3
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-09-01 22:42:18 +0100

    2019 day 20 squashed Modify search algorithm to go teleporter to teleporter rather than a step at a time. Everything uses the new search templates.

--- AoC2019/Puzzle20.cpp
+++ AoC2019/Puzzle20.cpp
@@ -2,218 +2,475 @@
 
 using namespace std;
 
-static string_view dummy =
-R"()";
-
 namespace Puzzle20_2019_Types
 {
+	template <typename TYPE>
+	class FromTo
+	{
+	public:
+		FromTo() = default;
+		FromTo(const FromTo&) = default;
+
+		FromTo(int32_t size, const TYPE& value)
+			: Size(size)
+		{
+			Data.resize(Size * Size, value);
+		}
+
+		TYPE& operator()(int32_t from, int32_t to)
+		{
+			int32_t index = (from * Size) + to;
+			return Data[index];
+		}
+
+		const TYPE& operator()(int32_t from, int32_t to) const
+		{
+			int32_t index = (from * Size) + to;
+			return Data[index];
+		}
+
+		const TYPE* GetRow(int32_t from) const
+		{
+			int32_t index = (from * Size);
+			return &Data[index];
+		}
+
+		int32_t GetSize() const
+		{
+			return Size;
+		}
+
+	private:
+		int32_t Size = 0;
+		vector<TYPE> Data;
+	};
+
+	using Label = pair<char, char>;
+
+	struct Teleporter
+	{
+		Vec2Int Location;
+		Label Label;
+		bool IsOuter;
+	};
+
 	struct Puzzle
 	{
-		ArrayMap2D Maze;
-		map<Point2, Point2> Teleporters;
-		map<Point2, string> DebugLabels;
-		Point2 Start;
-		Point2 End;
+		vector<Teleporter> Teleporters;
+		int32_t StartIndex;
+		int32_t EndIndex;
+		FromTo<int32_t> TeleporterToTeleporterDistances;
+	};
+
+	struct TeleportToTeleportFinder
+	{
+		using State = Vec2Int;
+
+		FromTo<int32_t>* TeleporterToTeleporterDistances = nullptr;
+
+		const uArrayMap2D* Maze = nullptr;
+		const HashMap<Vec2Int, int32_t>* TeleporterLocations = nullptr;
+
+		Vec2Int Start;
+		int32_t FromIndex;
+		vector<int32_t> AlreadyQueued;
+		SmallVector<Vec2Int, 4> Neighbours;
+
+		size_t MaxQueueSize() const;
+
+		void InitialiseQueuedChecker();
+		void HasQueued(const Vec2Int& s);
+		bool HasAlreadyQueued(const Vec2Int& s);
+
+		BFSControl ProcessCurrent(const Vec2Int& s, int32_t steps);
+
+		void CacheNeighbours(const Vec2Int& s);
+		const SmallVector<Vec2Int, 4>& GetNeighbours() const;
+		bool CanMoveTo(const Vec2Int& s);
+	};
+
+	struct FlatFinder
+	{
+		using State = int32_t;
+
+		const Puzzle& P;
+		int32_t Start;
+		int32_t End;
+		vector<pair<int32_t, int32_t>> Neighbours;
+		HashSet<int32_t>* Visited = nullptr;
+
+		size_t MaxQueueSize() const;
+		
+		void InitialiseVisitedChecker();
+		DijkstraControl Visit(const int32_t& s);
+		
+		void CacheNeighbours(const int32_t& s);
+		const vector<pair<int32_t, int32_t>>& GetNeighbours() const;
+		bool CanMoveTo(const int32_t& s);
+	};
+
+	struct RecursiveFinder
+	{
+		using State = pair<int32_t, int32_t>;
+
+		const Puzzle& P;
+		pair<int32_t, int32_t> Start;
+		pair<int32_t, int32_t> End;
+		vector<pair<int32_t, pair<int32_t, int32_t>>> Neighbours;
+		HashSet<pair<int32_t, int32_t>>* Visited = nullptr;
+
+		size_t MaxQueueSize() const;
+
+		void InitialiseVisitedChecker();
+		DijkstraControl Visit(const pair<int32_t, int32_t>& s);
+
+		void CacheNeighbours(const pair<int32_t, int32_t>& s);
+		const vector<pair<int32_t, pair<int32_t, int32_t>>>& GetNeighbours() const;
+		bool CanMoveTo(const pair<int32_t, int32_t>& s);
 	};
 }
 
 using namespace Puzzle20_2019_Types;
 
-static string ParseLabel(const ArrayMap2D &maze, const Point2& adjacentChar)
+template <>
+struct std::hash<pair<int32_t, int32_t>>
 {
-	string label;
-	for (const Point2& dir : array<Point2, 2>{ Point2::Left(), Point2::Up() })
+	size_t operator()(const pair<int32_t, int32_t>& p) const noexcept
+	{
+		uint32_t hash = 0x811c9dc5;
+
+		hash ^= p.first;
+		hash *= 0x01000193;
+		hash ^= p.second;
+		hash *= 0x01000193;
+
+		return hash;
+	}
+};
+
+static bool IsOuterTeleporter(const uArrayMap2D& maze, const Vec2Int& teleporter)
+{
+	return (teleporter.X == 2)
+		|| (teleporter.Y == 2)
+		|| (teleporter.X == maze.GetWidth() - 3)
+		|| (teleporter.Y == maze.GetHeight() - 3);
+}
+
+static Label ParseLabel(const uArrayMap2D &maze, const Vec2Int& adjacentChar)
+{
+	Label label;
+	for (const Vec2Int& dir : array<Vec2Int, 2>{ Vec2Int::Left(), Vec2Int::Up() })
 	{
 		if (isalpha(maze(adjacentChar + dir)))
 		{
-			label += maze(adjacentChar + dir);
-			label += maze(adjacentChar);
+			label = { maze(adjacentChar + dir), maze(adjacentChar) };
 		}
 	}
-	for (const Point2& dir : array<Point2, 2>{ Point2::Right(), Point2::Down() })
+	for (const Vec2Int& dir : array<Vec2Int, 2>{ Vec2Int::Right(), Vec2Int::Down() })
 	{
 		if (isalpha(maze(adjacentChar + dir)))
 		{
-			label += maze(adjacentChar);
-			label += maze(adjacentChar + dir);
+			label = { maze(adjacentChar), maze(adjacentChar + dir) };
 		}
 	}
 	return label;
 }
 
-static Puzzle ParsePuzzle(istream& input)
-{
-	Puzzle puzzle{ ReadArrayMap(input) };
+static void FindTeleporterToTeleporterDistances(const uArrayMap2D& maze, Puzzle* puzzle);
 
-	map<string, vector<Point2>> labels;
-	for (const auto& p : puzzle.Maze.Grid())
+static Puzzle ParsePuzzle()
+{
+	Puzzle puzzle;
+	puzzle.TeleporterToTeleporterDistances = FromTo<int32_t>(64, -1);
+
+	size_t mazeBufferSize = 128 * 128;
+	uArrayMap2D maze = ReaduArrayMap_PresizedHeap(static_cast<char*>(malloc(mazeBufferSize)), mazeBufferSize);
+	for (const auto& p : maze.Grid())
 	{
 		if (p.second == '.')
 		{
-			for (const auto& dir : Point2::CardinalDirections())
+			for (const auto& dir : Vec2Int::CardinalDirections())
 			{
-				Point2 neighbour = p.first + dir;
-				if (isalpha(puzzle.Maze(neighbour)))
+				Vec2Int neighbour = p.first + dir;
+				if (isalpha(maze(neighbour)))
 				{
-					string label = ParseLabel(puzzle.Maze, neighbour);
-					labels[label].push_back(p.first);
-					puzzle.DebugLabels[p.first] = label;
-					if (label == "AA"sv)
+					Label label = ParseLabel(maze, neighbour);
+					if (label == Label{ 'A', 'A' })
 					{
-						puzzle.Start = p.first;
+						puzzle.StartIndex = static_cast<int32_t>(puzzle.Teleporters.size());
 					}
-					else if (label == "ZZ"sv)
+					if (label == Label{ 'Z', 'Z' })
 					{
-						puzzle.End = p.first;
+						puzzle.EndIndex = static_cast<int32_t>(puzzle.Teleporters.size());
 					}
+
+					puzzle.Teleporters.push_back({ p.first, label, IsOuterTeleporter(maze, p.first) });
 				}
 			}
 		}
 	}
 
-	for (const auto& label : labels | views::filter([](const auto& l) { return l.second.size() == 2; }))
-	{
-		puzzle.Teleporters[label.second[0]] = label.second[1];
-		puzzle.Teleporters[label.second[1]] = label.second[0];
-	}
+	FindTeleporterToTeleporterDistances(maze, &puzzle);
 
 	return puzzle;
 }
 
-static bool IsOuterTeleporter(const Puzzle& puzzle, const Point2& teleporter)
+// ----------------------------------------------------------------------------
+
+size_t TeleportToTeleportFinder::MaxQueueSize() const
 {
-	return (teleporter.X == 2)
-		|| (teleporter.Y == 2)
-		|| (teleporter.X == puzzle.Maze.GetWidth() - 3)
-		|| (teleporter.Y == puzzle.Maze.GetHeight() - 3);
+	return 32;
 }
 
-static int64_t ShortestPath(const Puzzle& puzzle)
+void TeleportToTeleportFinder::InitialiseQueuedChecker()
 {
-	vector<pair<Point2, int64_t>> searchQueue{ { puzzle.Start, 0 } };
-	set<Point2> visited;
-	for (size_t i = 0; i < searchQueue.size(); i++)
+	AlreadyQueued.clear();
+	AlreadyQueued.resize(Maze->GetWidth() * Maze->GetHeight(), 0);
+}
+
+void TeleportToTeleportFinder::HasQueued(const Vec2Int& s)
+{
+	size_t index = (s.Y * Maze->GetWidth()) + s.X;
+	AlreadyQueued[index] = 1;
+}
+
+bool TeleportToTeleportFinder::HasAlreadyQueued(const Vec2Int& s)
+{
+	size_t index = (s.Y * Maze->GetWidth()) + s.X;
+	return AlreadyQueued[index];
+}
+
+BFSControl TeleportToTeleportFinder::ProcessCurrent(const Vec2Int& s, int32_t steps)
+{
+	if (s != Start)
 	{
-		auto [currentPos, currentSteps] = searchQueue[i];
-
-		if (currentPos == puzzle.End)
-			return currentSteps;
-
-		if (visited.insert(currentPos).second == false)
-			continue;
-
-		for (const Point2& dir : Point2::CardinalDirections())
+		int32_t teleporterIndex = -1;
+		if (TeleporterLocations->TryFind(s, &teleporterIndex))
 		{
-			Point2 neighbour = currentPos + dir;
-			if (puzzle.Maze(neighbour) == '.')
-			{
-				searchQueue.push_back({ neighbour, currentSteps + 1 });
-			}
-		}
-
-		const auto teleporter = puzzle.Teleporters.find(currentPos);
-		if (teleporter != puzzle.Teleporters.end())
-		{
-			searchQueue.push_back({ teleporter->second, currentSteps + 1 });
+			(*TeleporterToTeleporterDistances)(FromIndex, teleporterIndex) = steps;
+			(*TeleporterToTeleporterDistances)(teleporterIndex, FromIndex) = steps;
+			return BFSControl::Skip;
 		}
 	}
 
-	return -1;
+	return BFSControl::Process;
 }
 
-static int64_t ShortestPathRecursive(const Puzzle& puzzle)
+void TeleportToTeleportFinder::CacheNeighbours(const Vec2Int& s)
 {
-	const Vector3 start{ puzzle.Start.X, puzzle.Start.Y, 0 };
-	const Vector3 end{ puzzle.End.X, puzzle.End.Y, 0 };
-
-	vector<pair<Vector3, int64_t>> searchQueue{ { start, 0 } };
-	set<Vector3> visited;
-
-	map<Vector3, Vector3> from;
-
-	for (size_t i = 0; i < searchQueue.size(); i++)
+	Neighbours.Clear();
+	for (const Vec2Int& dir : Vec2Int::CardinalDirections())
 	{
-		auto [currentPos, currentSteps] = searchQueue[i];
-		if (currentPos == end)
-			return currentSteps;
+		Neighbours.PushBack(s + dir);
+	}
+}
 
-		if (visited.insert(currentPos).second == false)
-			continue;
+const SmallVector<Vec2Int, 4>& TeleportToTeleportFinder::GetNeighbours() const
+{
+	return Neighbours;
+}
 
-		for (const Point2& dir : Point2::CardinalDirections())
+bool TeleportToTeleportFinder::CanMoveTo(const Vec2Int& s)
+{
+	return (*Maze)(s) == '.';
+}
+
+static void FindTeleporterToTeleporterDistances(const uArrayMap2D& maze, Puzzle* puzzle)
+{
+	BFS<TeleportToTeleportFinder> bfs;
+	bfs.TeleporterToTeleporterDistances = &puzzle->TeleporterToTeleporterDistances;
+	bfs.Maze = &maze;
+
+	HashMap<Vec2Int, int32_t> teleporterLocations(128, Vec2Int::Min());
+	for (int32_t i = 0; i < static_cast<int32_t>(puzzle->Teleporters.size()); i++)
+	{
+		teleporterLocations.Insert(puzzle->Teleporters[i].Location, i);
+	}
+	bfs.TeleporterLocations = &teleporterLocations;
+
+	for (int32_t i = 0; i < static_cast<int32_t>(puzzle->Teleporters.size()); i++)
+	{
+		bfs.FromIndex = i;
+		bfs.Start = puzzle->Teleporters[i].Location;
+		bfs.FindAll(puzzle->Teleporters[i].Location);
+	}
+}
+
+// ----------------------------------------------------------------------------
+
+size_t FlatFinder::MaxQueueSize() const
+{
+	return 32;
+}
+
+void FlatFinder::InitialiseVisitedChecker()
+{
+}
+
+DijkstraControl FlatFinder::Visit(const int32_t& s)
+{
+	if (s == P.EndIndex)
+		return DijkstraControl::Found;
+
+	if (Visited->Insert(s) == false)
+		return DijkstraControl::Skip;
+
+	return DijkstraControl::Process;
+}
+
+void FlatFinder::CacheNeighbours(const int32_t& from)
+{
+	Neighbours.clear();
+	const int32_t *to = P.TeleporterToTeleporterDistances.GetRow(from);
+	for (int32_t i = 0; i < P.TeleporterToTeleporterDistances.GetSize(); i++)
+	{
+		if (to[i] != -1)
 		{
-			Point2 neighbour = Point2{ currentPos.X, currentPos.Y } + dir;
-			if (puzzle.Maze(neighbour) == '.')
-			{
-				Vector3 next{ neighbour.X, neighbour.Y, currentPos.Z };
-				from.insert({ next, currentPos });
-				searchQueue.push_back({ next, currentSteps + 1 });
-			}
+			Neighbours.push_back({ to[i], i });
 		}
+	}
+}
 
-		const auto teleporter = puzzle.Teleporters.find(Point2{ currentPos.X, currentPos.Y });
-		if (teleporter != puzzle.Teleporters.end())
+const vector<pair<int32_t, int32_t>>& FlatFinder::GetNeighbours() const
+{
+	return Neighbours;
+}
+
+bool FlatFinder::CanMoveTo(const int32_t&)
+{
+	return true;
+}
+
+static int32_t ShortestPath(const Puzzle& puzzle)
+{
+	Dijkstra<FlatFinder> dijkstra{ puzzle };
+	HashSet<int32_t> visited(128, -1);
+	dijkstra.Visited = &visited;
+	dijkstra.Neighbours.reserve(16);
+
+	dijkstra.Start = puzzle.StartIndex;
+	dijkstra.End = puzzle.EndIndex;
+
+	int32_t shortest = dijkstra.FindShortest(puzzle.StartIndex);
+
+	return shortest;
+}
+
+// ----------------------------------------------------------------------------
+
+size_t RecursiveFinder::MaxQueueSize() const
+{
+	return 128;
+}
+
+void RecursiveFinder::InitialiseVisitedChecker()
+{
+}
+
+DijkstraControl RecursiveFinder::Visit(const pair<int32_t, int32_t>& s)
+{
+	if (s == End)
+		return DijkstraControl::Found;
+
+	if (Visited->Insert(s) == false)
+		return DijkstraControl::Skip;
+
+	return DijkstraControl::Process;
+}
+
+void RecursiveFinder::CacheNeighbours(const pair<int32_t, int32_t>& from)
+{
+	Neighbours.clear();
+	const int32_t* to = P.TeleporterToTeleporterDistances.GetRow(from.first);
+	for (int32_t i = 0; i < P.TeleporterToTeleporterDistances.GetSize(); i++)
+	{
+		if (to[i] != -1)
 		{
-			if (IsOuterTeleporter(puzzle, Point2{ currentPos.X, currentPos.Y }))
+			if (to[i] == 0)
 			{
-				if (currentPos.Z > 0)
+				// Up and down connections
+				if (P.Teleporters[from.first].IsOuter)
 				{
-					Vector3 next{ teleporter->second.X, teleporter->second.Y, currentPos.Z - 1 };
-					from.insert({ next, currentPos });
-					searchQueue.push_back({ next, currentSteps + 1 });
+					if (from.second > 0)
+					{
+						Neighbours.push_back({ 1, { i, from.second - 1 } });
+					}
+				}
+				else
+				{
+					Neighbours.push_back({ 1, { i, from.second + 1 } });
 				}
 			}
 			else
 			{
-				Vector3 next{ teleporter->second.X, teleporter->second.Y, currentPos.Z + 1 };
-				from.insert({ next, currentPos });
-				searchQueue.push_back({ next, currentSteps + 1 });
+				// Same level connections
+				Neighbours.push_back({ to[i], { i, from.second } });
 			}
 		}
 	}
-
-	return -1;
 }
 
-static void Puzzle20_A(const string &filename)
+const vector<pair<int32_t, pair<int32_t, int32_t>>>& RecursiveFinder::GetNeighbours() const
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	Puzzle puzzle = ParsePuzzle(input);
-
-	int64_t answer = ShortestPath(puzzle);
-
-	printf("[2019] Puzzle20_A: %" PRId64 "\n", answer);
+	return Neighbours;
 }
 
-
-static void Puzzle20_B(const string& filename)
+bool RecursiveFinder::CanMoveTo(const pair<int32_t, int32_t>&)
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	Puzzle puzzle = ParsePuzzle(input);
-
-	int64_t answer = ShortestPathRecursive(puzzle);
-
-	printf("[2019] Puzzle20_B: %" PRId64 "\n", answer);
+	return true;
 }
+
+static int32_t ShortestPathRecursive(const Puzzle& puzzle)
+{
+	Dijkstra<RecursiveFinder> dijkstra{ puzzle };
+	HashSet<pair<int32_t, int32_t>> visited(16 * 1024, { -1, -1 });
+	dijkstra.Visited = &visited;
+	dijkstra.Neighbours.reserve(16);
+
+	dijkstra.Start = { puzzle.StartIndex, 0 };
+	dijkstra.End = { puzzle.EndIndex, 0 };
+
+	int32_t shortest = dijkstra.FindShortest(dijkstra.Start);
+
+	return shortest;
+}
+
+// ----------------------------------------------------------------------------
 
 void Puzzle20_A_2019()
 {
-	Puzzle20_A(R"(z:\AoCInput\2019\Puzzle20.txt)");
+	Puzzle puzzle = ParsePuzzle();
 
-	int32_t answer = 0;
+	// Connect the inner and outer teleporters
+	for (const auto& p : AllUnorderedPairs(puzzle.Teleporters.size()))
+	{
+		if (puzzle.Teleporters[p.first].Label == puzzle.Teleporters[p.second].Label)
+		{
+			puzzle.TeleporterToTeleporterDistances((int32_t)p.first, (int32_t)p.second) = 1;
+			puzzle.TeleporterToTeleporterDistances((int32_t)p.second, (int32_t)p.first) = 1;
+		}
+	}
+
+	int32_t answer = ShortestPath(puzzle);
+
 	PuzzleOutput::Submit(2019, 20, 1, answer);
 }
 
 void Puzzle20_B_2019()
 {
-	Puzzle20_B(R"(z:\AoCInput\2019\Puzzle20.txt)");
+	Puzzle puzzle = ParsePuzzle();
 
-	int32_t answer = 0;
+	// Connect the inner and outer teleporters
+	for (const auto& p : AllUnorderedPairs(puzzle.Teleporters.size()))
+	{
+		if (puzzle.Teleporters[p.first].Label == puzzle.Teleporters[p.second].Label)
+		{
+			puzzle.TeleporterToTeleporterDistances((int32_t)p.first, (int32_t)p.second) = 0;
+			puzzle.TeleporterToTeleporterDistances((int32_t)p.second, (int32_t)p.first) = 0;
+		}
+	}
+
+	int32_t answer = ShortestPathRecursive(puzzle);
+
 	PuzzleOutput::Submit(2019, 20, 2, answer);
 }
