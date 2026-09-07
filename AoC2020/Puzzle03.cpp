4130bb49716ace686e5de4b894e7dc822a8acd70
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-09-07 21:50:06 +0100

    2020 day 3 squashed

--- AoC2020/Puzzle03.cpp
+++ AoC2020/Puzzle03.cpp
@@ -2,56 +2,49 @@
 
 using namespace std;
 
-static string_view dummy =
-R"()";
-
 namespace Puzzle03_2020_Types
 {
 }
 
 using namespace Puzzle03_2020_Types;
 
-
-static void Puzzle03_A(const string &filename)
+void Puzzle03_A_2020()
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
+	vector<string> trees;
+	trees.reserve(330);
+	Parse::ReadAllLines(&trees, 32);
 
-	int64_t answer = 0;
-
-	vector<string> trees = ReadAllLines(input);
 	size_t toboggan = 0;
+
+	int32_t answer = 0;
 	for (size_t row = 0; row < trees.size(); row++)
 	{
 		answer += (trees[row][toboggan] == '#' ? 1 : 0);
 		toboggan = (toboggan + 3) % trees[row].size();
 	}
 
-	printf("[2020] Puzzle03_A: %" PRId64 "\n", answer);
+	PuzzleOutput::Submit(2020, 3, 1, answer);
 }
 
-static void Puzzle03_B(const string& filename)
+void Puzzle03_B_2020()
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
+	vector<string> trees;
+	trees.reserve(330);
+	Parse::ReadAllLines(&trees, 32);
 
-	vector<string> trees = ReadAllLines(input);
-
-	const vector<Point2> slopes =
+	const vector<Vec2Int> slopes =
 	{
-		Point2{ 1, 1 },
-		Point2{ 3, 1 },
-		Point2{ 5, 1 },
-		Point2{ 7, 1 },
-		Point2{ 1, 2 },
+		Vec2Int{ 1, 1 },
+		Vec2Int{ 3, 1 },
+		Vec2Int{ 5, 1 },
+		Vec2Int{ 7, 1 },
+		Vec2Int{ 1, 2 },
 	};
 
-	vector<int64_t> counts;
-	for (const Point2& slope : slopes)
+	uint64_t answer = 1;
+	for (const Vec2Int& slope : slopes)
 	{
-		int64_t treesEncountered = 0;
+		int32_t treesEncountered = 0;
 
 		size_t toboggan = 0;
 		for (size_t row = 0; row < trees.size(); row += slope.Y)
@@ -60,30 +53,8 @@ static void Puzzle03_B(const string& filename)
 			toboggan = (toboggan + slope.X) % trees[row].size();
 		}
 
-		counts.push_back(treesEncountered);
+		answer *= treesEncountered;
 	}
 
-	int64_t answer = 1;
-	for (int64_t c : counts)
-	{
-		answer *= c;
-	}
-
-	printf("[2020] Puzzle03_B: %" PRId64 "\n", answer);
-}
-
-void Puzzle03_A_2020()
-{
-	Puzzle03_A(R"(z:\AoCInput\2020\Puzzle03.txt)");
-
-	int32_t answer = 0;
-	PuzzleOutput::Submit(2020, 3, 1, answer);
-}
-
-void Puzzle03_B_2020()
-{
-	Puzzle03_B(R"(z:\AoCInput\2020\Puzzle03.txt)");
-
-	int32_t answer = 0;
 	PuzzleOutput::Submit(2020, 3, 2, answer);
 }
