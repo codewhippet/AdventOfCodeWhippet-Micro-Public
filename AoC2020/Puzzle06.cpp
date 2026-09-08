d6b722fd0d61c8fbd9e80e5e2714bff3c5d13259
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-09-08 09:59:50 +0100

    2020 day 6 squashed Eliminated intermediate data structures: reading all lines into memory, keeping individual answer contributions before accumulating, etc...

--- AoC2020/Puzzle06.cpp
+++ AoC2020/Puzzle06.cpp
@@ -2,100 +2,66 @@
 
 using namespace std;
 
-static string_view dummy =
-R"()";
-
 namespace Puzzle06_2020_Types
 {
 }
 
 using namespace Puzzle06_2020_Types;
 
-
-static void Puzzle06_A(const string &filename)
-{
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	vector<string> lines = ReadAllLines(input);
-	lines.push_back("");
-
-	set<char> seen;
-	vector<size_t> groupCounts;
-	for (const string& line : lines)
-	{
-		if (line.empty())
-		{
-			groupCounts.push_back(seen.size());
-			seen.clear();
-		}
-
-		for (char c : line)
-		{
-			seen.insert(c);
-		}
-	}
-
-	int64_t answer = accumulate(groupCounts.begin(), groupCounts.end(), 0ll);
-
-	printf("[2020] Puzzle06_A: %" PRId64 "\n", answer);
-}
-
-static void Puzzle06_B(const string& filename)
-{
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	vector<string> lines = ReadAllLines(input);
-	lines.push_back("");
-
-	int numPeople = 0;
-	map<char, int> seen;
-	vector<size_t> groupCounts;
-	for (const string& line : lines)
-	{
-		if (line.empty())
-		{
-			size_t allAnswered = count_if(seen.begin(), seen.end(),
-				[&seen, numPeople](const pair<char, int>& kvp)
-				{
-					return kvp.second == numPeople;
-				});
-
-			groupCounts.push_back(allAnswered);
-			seen.clear();
-			numPeople = 0;
-
-			continue;
-		}
-
-		for (char c : line)
-		{
-			seen[c]++;
-		}
-
-		numPeople++;
-	}
-
-	int64_t answer = accumulate(groupCounts.begin(), groupCounts.end(), 0ll);
-
-	printf("[2020] Puzzle06_B: %" PRId64 "\n", answer);
-}
-
 void Puzzle06_A_2020()
 {
-	Puzzle06_A(R"(z:\AoCInput\2020\Puzzle06.txt)");
-
 	int32_t answer = 0;
+
+	array<int32_t, 26> seen = {};
+	while (PuzzleInput::PeekChar() != EOF)
+	{
+		if (PuzzleInput::PeekChar() == '\n')
+		{
+			answer += accumulate(seen.begin(), seen.end(), 0);
+			ranges::fill(seen, 0);
+		}
+
+		for (char c : Parse::ReadUntilSeen('\n'))
+		{
+			seen[c - 'a'] = 1;
+		}
+	}
+
+	answer += accumulate(seen.begin(), seen.end(), 0);
+
 	PuzzleOutput::Submit(2020, 6, 1, answer);
 }
 
 void Puzzle06_B_2020()
 {
-	Puzzle06_B(R"(z:\AoCInput\2020\Puzzle06.txt)");
-
 	int32_t answer = 0;
+
+	int32_t numPeople = 0;
+	array<int32_t, 26> seen = {};
+	while (PuzzleInput::PeekChar() != EOF)
+	{
+		if (PuzzleInput::PeekChar() == '\n')
+		{
+			int32_t allAnswered = static_cast<int32_t>(count(seen.begin(), seen.end(), numPeople));
+			answer += allAnswered;
+
+			ranges::fill(seen, 0);
+			numPeople = 0;
+
+			PuzzleInput::DropChar();
+			continue;
+		}
+
+		for (char c : Parse::ReadUntilSeen('\n'))
+		{
+			seen[c - 'a']++;
+		}
+
+		numPeople++;
+	}
+
+	int32_t allAnswered = static_cast<int32_t>(count(seen.begin(), seen.end(), numPeople));
+	answer += allAnswered;
+
 	PuzzleOutput::Submit(2020, 6, 2, answer);
 }
