9b407878756a820eedc2e656bffda65f8e75b016
codewhippet <10889487+codewhippet@users.noreply.github.com>
2026-09-17 15:30:42 +0100

    2020 day 16 squashed Mostly parsing changes. Swap maps for pre-sized vectors.

--- AoC2020/Puzzle16.cpp
+++ AoC2020/Puzzle16.cpp
@@ -2,159 +2,179 @@
 
 using namespace std;
 
-static string_view dummy =
-R"()";
-
 namespace Puzzle16_2020_Types
 {
 	struct TicketInfo 
 	{
-		vector<pair<string, function<bool(int64_t)>>> FieldRules;
-		vector<int64_t> MyTicket;
-		vector<vector<int64_t>> NearbyTickets;
+		vector<pair<string, function<bool(uint32_t)>>> FieldRules;
+		vector<uint32_t> MyTicket;
+		vector<vector<uint32_t>> NearbyTickets;
 	};
 }
 
 using namespace Puzzle16_2020_Types;
 
-static function<bool(int64_t)> MakeRule(int64_t a, int64_t b, int64_t c, int64_t d)
+static function<bool(uint32_t)> MakeRule(uint32_t a, uint32_t b, uint32_t c, uint32_t d)
 {
-	return [a, b, c, d](int64_t v) { return ((v >= a) && (v <= b)) || ((v >= c) && (v <= d)); };
+	return [a, b, c, d](uint32_t v) { return ((v >= a) && (v <= b)) || ((v >= c) && (v <= d)); };
 }
 
-static TicketInfo ReadPuzzle(istream& input)
+static TicketInfo ReadPuzzle()
 {
 	TicketInfo info;
 
-	vector<string> lines = ReadAllLines(input);
-
-	size_t lineNo = 0;
-
-	regex ruleFormat(R"(([^:]+): (\d+)-(\d+) or (\d+)-(\d+))");
-	while (lines[lineNo].empty() == false)
+	info.FieldRules.reserve(20);
+	while (PuzzleInput::PeekChar() != '\n')
 	{
-		smatch ruleMatch;
-		bool shouldMatch = regex_match(lines[lineNo], ruleMatch, ruleFormat);
-		assert(shouldMatch);
-		(void)shouldMatch;
-		function<bool(int64_t)> validityRule = MakeRule(
-			stoll(ruleMatch[2]),
-			stoll(ruleMatch[3]),
-			stoll(ruleMatch[4]),
-			stoll(ruleMatch[5]));
-		info.FieldRules.push_back(make_pair(ruleMatch[1].str(), move(validityRule)));
-		lineNo++;
+		string label;
+		label.reserve(32);
+		for (char c : Parse::ReadUntilSeen(':'))
+			label += c;
+
+		uint32_t a = Parse::GetUint32();
+		uint32_t b = Parse::GetUint32();
+		uint32_t c = Parse::GetUint32();
+		uint32_t d = Parse::GetUint32();
+		info.FieldRules.push_back({ label, MakeRule(a, b, c, d) });
+		Parse::DiscardExpected("\n");
 	}
 
-	lineNo += 2;
-	info.MyTicket = MakeEnumerator(StringSplit(lines[lineNo], ','))
-		->Select<int64_t>([](const string& s) { return stoll(s); })
-		->ToVector();
+	Parse::DiscardExpected("\nyour ticket:\n");
 
-	lineNo += 3;
-	while (lineNo < lines.size())
+	info.MyTicket.reserve(20);
+	while (PuzzleInput::PeekChar() != '\n')
 	{
-		info.NearbyTickets.push_back(
-			MakeEnumerator(StringSplit(lines[lineNo], ','))
-				->Select<int64_t>([](const string& s) { return stoll(s); })
-				->ToVector());
-		lineNo++;
+		info.MyTicket.push_back(Parse::GetUint32());
+	}
+
+	Parse::DiscardExpected("\n\nnearby tickets:\n");
+
+	info.NearbyTickets.reserve(256);
+	while (PuzzleInput::NextLine())
+	{
+		vector<uint32_t> ticket;
+		ticket.reserve(20);
+		while (PuzzleInput::PeekChar() != '\n')
+		{
+			ticket.push_back(Parse::GetUint32());
+		}
+		info.NearbyTickets.push_back(move(ticket));
 	}
 
 	return info;
 }
 
-static void Puzzle16_A(const string &filename)
+static bool TryGetUint32(uint32_t* out)
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
-
-	TicketInfo info = ReadPuzzle(input);
-
-	vector<int64_t> invalidValues;
-	for (const vector<int64_t>& ticket : info.NearbyTickets)
+	while (!isdigit(PuzzleInput::PeekChar()))
 	{
-		for (int64_t value : ticket)
+		int c = PuzzleInput::GetChar();
+		if (c == EOF)
+			return false;
+	}
+
+	int32_t result = 0;
+	while (isdigit(PuzzleInput::PeekChar()))
+	{
+		result = (result * 10) + static_cast<int32_t>(PuzzleInput::GetChar() - '0');
+	}
+
+	*out = result;
+	return true;
+}
+
+void Puzzle16_A_2020()
+{
+	vector<function<bool(uint32_t)>> fieldRules;
+	fieldRules.reserve(20);
+
+	while (PuzzleInput::PeekChar() != '\n')
+	{
+		uint32_t a = Parse::GetUint32();
+		uint32_t b = Parse::GetUint32();
+		uint32_t c = Parse::GetUint32();
+		uint32_t d = Parse::GetUint32();
+		fieldRules.push_back(MakeRule(a, b, c, d));
+		Parse::DiscardExpected("\n");
+	}
+
+	Parse::DiscardExpected("\nyour ticket:\n");
+	PuzzleInput::DropLine();
+	Parse::DiscardExpected("\n\nnearby tickets:\n");
+
+	int32_t answer = 0;
+
+	uint32_t value;
+	while (TryGetUint32(&value))
+	{
+		bool invalid = ranges::none_of(fieldRules, [&](const auto& rule) { return rule(value); });
+		if (invalid)
 		{
-			bool isValid = false;
-			for (vector<pair<string, function<bool(int64_t)>>>::const_reference validityCheck : info.FieldRules)
-			{
-				if (validityCheck.second(value))
-				{
-					isValid = true;
-					break;
-				}
-			}
-			if (isValid == false)
-			{
-				invalidValues.push_back(value);
-			}
+			answer += value;
 		}
 	}
 
-	int64_t answer = accumulate(invalidValues.begin(), invalidValues.end(), 0ll);
-
-	printf("[2020] Puzzle16_A: %" PRId64 "\n", answer);
+	PuzzleOutput::Submit(2020, 16, 1, answer);
 }
 
-static void Puzzle16_B(const string& filename)
+void Puzzle16_B_2020()
 {
-	(void)filename;
-	ifstream input(filename);
-	//istringstream input(dummy);
+	TicketInfo info = ReadPuzzle();
 
-	TicketInfo info = ReadPuzzle(input);
-
-	map<size_t, vector<int64_t>> ticketValidityMasks;
+	vector<vector<uint32_t>> ticketValidityMasks;
+	ticketValidityMasks.resize(info.NearbyTickets.size());
 	for (size_t ticketIndex = 0; ticketIndex < info.NearbyTickets.size(); ticketIndex++)
 	{
-		const vector<int64_t>& ticket = info.NearbyTickets[ticketIndex];
-		vector<int64_t> ticketValueValidity;
-		for (int64_t value : ticket)
+		const vector<uint32_t>& ticket = info.NearbyTickets[ticketIndex];
+
+		vector<uint32_t> ticketValueValidity;
+		ticketValueValidity.reserve(20);
+		for (uint32_t value : ticket)
 		{
-			int64_t validityMask = 0;
+			uint32_t validityMask = 0;
 			for (size_t fieldIndex = 0; fieldIndex < info.FieldRules.size(); fieldIndex++)
 			{
-				int64_t valid = info.FieldRules[fieldIndex].second(value) ? 1 : 0;
+				uint32_t valid = info.FieldRules[fieldIndex].second(value) ? 1 : 0;
 				validityMask |= (valid << fieldIndex);
 			}
 			ticketValueValidity.push_back(validityMask);
 		}
-		if (find(ticketValueValidity.begin(), ticketValueValidity.end(), 0) == ticketValueValidity.end())
+
+		if (find(ticketValueValidity.begin(), ticketValueValidity.end(), 0u) == ticketValueValidity.end())
 		{
-			ticketValidityMasks[ticketIndex] = move(ticketValueValidity);
+			ticketValidityMasks[ticketIndex].swap(ticketValueValidity);
 		}
 	}
 
-	vector<int64_t> globalValidity;
-	globalValidity.resize(ticketValidityMasks.begin()->second.size());
-	fill(globalValidity.begin(), globalValidity.end(), ~0ll);
+	vector<uint32_t> globalValidity;
+	globalValidity.resize(info.FieldRules.size());
+	fill(globalValidity.begin(), globalValidity.end(), ~0);
 
-	for (map<size_t, vector<int64_t>>::const_reference ticketValidity : ticketValidityMasks)
+	for (vector<vector<uint32_t>>::const_reference ticketValidity : ticketValidityMasks)
 	{
-		for (size_t valueIndex = 0; valueIndex < ticketValidity.second.size(); valueIndex++)
+		for (size_t valueIndex = 0; valueIndex < ticketValidity.size(); valueIndex++)
 		{
-			globalValidity[valueIndex] &= ticketValidity.second[valueIndex];
+			globalValidity[valueIndex] &= ticketValidity[valueIndex];
 		}
 	}
 
-	map<size_t, size_t> fieldIndexToValueIndex;
+	vector<size_t> fieldIndexToValueIndex;
+	fieldIndexToValueIndex.resize(info.FieldRules.size());
 	while (true)
 	{
-		vector<int64_t>::iterator canBeMapped = find_if(globalValidity.begin(), globalValidity.end(),
-			[](int64_t v) { return __popcnt64(v) == 1; });
+		vector<uint32_t>::iterator canBeMapped = find_if(globalValidity.begin(), globalValidity.end(),
+			[](uint32_t v) { return popcount(v) == 1; });
 		if (canBeMapped == globalValidity.end())
 		{
 			break;
 		}
 
-		int64_t validityMask = *canBeMapped;
+		uint32_t validityMask = *canBeMapped;
 
-		int64_t fieldIndex = _tzcnt_u64(validityMask);
+		uint32_t fieldIndex = countr_zero(validityMask);
 		fieldIndexToValueIndex[fieldIndex] = distance(globalValidity.begin(), canBeMapped);
 
-		for_each(globalValidity.begin(), globalValidity.end(), [validityMask](int64_t& v) { v &= ~validityMask; });
+		for_each(globalValidity.begin(), globalValidity.end(), [validityMask](uint32_t& v) { v &= ~validityMask; });
 	}
 
 	int64_t answer = 1;
@@ -166,21 +186,5 @@ static void Puzzle16_B(const string& filename)
 		}
 	}
 
-	printf("[2020] Puzzle16_B: %" PRId64 "\n", answer);
-}
-
-void Puzzle16_A_2020()
-{
-	Puzzle16_A(R"(z:\AoCInput\2020\Puzzle16.txt)");
-
-	int32_t answer = 0;
-	PuzzleOutput::Submit(2020, 16, 1, answer);
-}
-
-void Puzzle16_B_2020()
-{
-	Puzzle16_B(R"(z:\AoCInput\2020\Puzzle16.txt)");
-
-	int32_t answer = 0;
 	PuzzleOutput::Submit(2020, 16, 2, answer);
 }
